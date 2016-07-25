d4b0d607ed8b5e7b2813b21ff2dd71cfb3d14079
Zack Katz <zack@katzwebservices.com>
2016-07-25 15:39:50 -0600

    Sanitize the {preview} text.
    - Convert HTML entities so quotes no longer break stuff
    - Add a "…" at the end of the preview
    - Replace multiple spaces and replace with one (HS strips HTML tags and
    leaves whitespace)
    - Fix replacing multiple instances of a template tag
    
    @SDavisMedia I think fixing the `{preview}` is preferable to changing
    it; that way it's a bugfix rather than a point release. It's easy
    enough to modify the template using the
    `gf_helpscout_docs_script_settings` filter.
    
    #17 #18

--- gravity-forms-help-scout-search.php
+++ gravity-forms-help-scout-search.php
@@ -118,7 +118,7 @@ class PW_GF_HS_Search {
 			'template' => array(
 				'wrap_class' => 'docs-search-wrap',
 				'before' => '<ul class="docs-search-results">',
-				'item' => '<li class="article"><a href="{url}" title="{preview}" target="_blank">{name}</a></li>',
+				'item' => '<li class="article"><a href="{url}" title="{preview|esc}&hellip;" target="_blank">{name}</a></li>',
 				'after' => '</ul>',
 				'results_found' => '<span class="{css_class}">{text}</span>',
 			),
